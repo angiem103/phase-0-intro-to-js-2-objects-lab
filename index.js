128672579769ed9cd96e99722dd80a964dad2cd7
Angie <amaticorena103@hotmail.com>
2021-11-09 21:12:50 -0500

    commit tree 03f4f3456cd56941f1cee0570a86c90ca02787b4

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const employee = {
-    "name": "Alice",
-    "streetAddress": "123 Wonderland Ave"
-} 
+    name: '',
+    streetAddress:'',
+}
 
-function updateEmployeeWithKeyAndValue(employee,key,value){
-    const newEmployee = {...employee}
-    newEmployee [key]=value
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const newEmployee = {...employee};
+   newEmployee[key]=value;
+   return newEmployee
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value
+   return employee
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+   const newEmployee = {...employee}
+   delete newEmployee[key]
     return newEmployee
-}
+};
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
-    employee [key]=value
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
     return employee
 }
-
-function deleteFromEmployeeByKey(employee,key,value){
-    const lessEmployee = {...employee}
-    lessEmployee[key]=value
-    return lessEmployee
-}
-
-const newEmployee = deleteFromEmployeeByKey (lessEmployee,key,value)
-
-function destructivelyDeleteFromEmployeeByKey(employee,key,value){
-    employee [key]=value
-    return employee
-}
-
-const newerEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value)
